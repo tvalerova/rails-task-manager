fc300133538a1342bc9d52228c77c5e78efb7449
Tereza Valerova <terezavalerova@gmail.com>
2020-03-05 10:53:55 +0100

    puma updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :tasks
+  # resources :tasks
   # read all
-  # get 'tasks', to: 'tasks#index'
-  # # get for a new task needs to be before read one
-  # # it needs to be created first
-  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  get 'tasks', to: 'tasks#index'
+  # get for a new task needs to be before read one
+  # it needs to be created first
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
 
-  # # read one
-  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # read one
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-  # # create-post request
-  # post 'tasks', to: 'tasks#create', as: 'create_task'
+  # create-post request
+  post 'tasks', to: 'tasks#create', as: 'create_task'
 
-  # # update
-  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  # patch 'tasks/:id', to: 'tasks#update', as: 'update_task'
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update_task'
 
-  # # delete
-  # delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
+  # delete
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
 end
